3c6904911379fa200949a7603d85cc1cc89f3667
thanhptse27997 <63510895+thanhptse27997@users.noreply.github.com>
2020-07-15 12:36:14 +0700

    Updates

---
+++ precache-manifest.6e47ce5f652097712a9fbe03f6bd91f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e4e9c9fe6a6399ec13ee0fa5667659",
+    "url": "/reactCv-shopping/index.html"
+  },
+  {
+    "revision": "b7cac5c106b8476e2466",
+    "url": "/reactCv-shopping/static/css/main.722bbe37.chunk.css"
+  },
+  {
+    "revision": "d961e83ca8cfb2ea5b66",
+    "url": "/reactCv-shopping/static/js/2.9142917e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactCv-shopping/static/js/2.9142917e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7cac5c106b8476e2466",
+    "url": "/reactCv-shopping/static/js/main.2aeda08f.chunk.js"
+  },
+  {
+    "revision": "415f942016a6b3c1f135",
+    "url": "/reactCv-shopping/static/js/runtime-main.fd08ea14.js"
+  }
+]);
