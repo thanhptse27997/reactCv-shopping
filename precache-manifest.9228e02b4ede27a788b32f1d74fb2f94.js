574d766e5fa121a00d63128c95eba299e8d1e89b
thanhptse27997 <63510895+thanhptse27997@users.noreply.github.com>
2020-08-10 23:44:41 +0700

    Updates

---
+++ precache-manifest.9228e02b4ede27a788b32f1d74fb2f94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bef2eec8f3578c3f117e4f566c1255c",
+    "url": "/reactCv-shopping/index.html"
+  },
+  {
+    "revision": "947f0bd9dfad91468260",
+    "url": "/reactCv-shopping/static/css/2.1c8fa9c2.chunk.css"
+  },
+  {
+    "revision": "da47578e35b87c2392c1",
+    "url": "/reactCv-shopping/static/css/main.ff70b0b6.chunk.css"
+  },
+  {
+    "revision": "947f0bd9dfad91468260",
+    "url": "/reactCv-shopping/static/js/2.36766b77.chunk.js"
+  },
+  {
+    "revision": "6020e2578ce61e153ee5130f44d173fc",
+    "url": "/reactCv-shopping/static/js/2.36766b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da47578e35b87c2392c1",
+    "url": "/reactCv-shopping/static/js/main.448f5488.chunk.js"
+  },
+  {
+    "revision": "415f942016a6b3c1f135",
+    "url": "/reactCv-shopping/static/js/runtime-main.fd08ea14.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactCv-shopping/precache-manifest.72d350901d051917d19553e939df4b6b.js"
+  "/reactCv-shopping/precache-manifest.9228e02b4ede27a788b32f1d74fb2f94.js"
 );
 
 self.addEventListener('message', (event) => {
